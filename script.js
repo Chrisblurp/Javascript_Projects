2462646df92b2986dd6daa1af7bd6ac6b5d9ced3
Chrisblurp <adeyemi504@gmail.com>
2022-06-20 23:11:40 +0100

    updated

---
+++ script.js
@@ -0,0 +1,4 @@
+let myName = ("Adeyemi Chris");
+let myHeight = (1.78);
+let myCountry = ("Nigeria");
+console.log ("My names are " + myName + ", i am " + myHeight + "m," + " i am from " + myCountry);
