919288a7b696e987e5926d503aacd7eac618dcb1
Chrisblurp <adeyemi504@gmail.com>
2022-06-23 00:48:43 +0100

    updated

---
+++ calculator.js
@@ -0,0 +1,36 @@
+// program for a simple calculator
+let result;
+
+// take the operator input
+
+
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+const number2 = parseFloat(prompt('Enter second number: '));
+
+switch(operator) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
+
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}
